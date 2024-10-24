075b93191b63f9b70b01a220253d71f3b4508d11
ekankam <eb.kankam26@gmail.com>
2024-10-24 13:03:03 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen w-full bg-[url('./assets/images/background-pattern-mobile.svg')] md:bg-[url('./assets/images/background-pattern-desktop.svg')] bg-no-repeat bg-contain bg-light-pink">
+      <h1>Hello, World!</h1>
+    </main>
+  );
+}
+
+export default App;
